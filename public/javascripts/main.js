cffdedafad909f9e563c92e848396e6e412952a5
Andrew Johnson <LimeyJohnson@gmail.com>
2016-05-30 06:13:42 -0700

    20% of conversion done

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,35 @@
-angular.module('hurleyisms', [])
-  .controller('MainController', ['$scope', '$http', function ($scope, $http) {
-      $scope.Lines = [];
-      $scope.GetLines = function () {
-          $http.get('admin/data').then(function (data) {
-              $scope.Lines = data.data;
-          });
+var hurleyApp = angular.module('hurleyisms', []);
+
+hurleyApp.controller('MainController', ['$scope', '$http', function ($scope, $http) {
+    $scope.Lines = [];
+    /*****************************************
+                   DataAccess 
+    *****************************************/
+    function updateCache(callback) {
+        $.getJSON('/data/' + audience + '/' + profanity, function (data) {
+            cachedLines = data;
+            callback();
+        });
+    }
+    function sendLine(line, callback) {
+        $.post('/add', line, function (data) {
+            callback();
+        });
+    }
+    function rate(id, rating, callback) {
+        $.getJSON('/rate/' + id + '/' + rating, function (data) {
+            callback();
+        });
+    }
+    $scope.View = 1;
+    $scope.profanity = false;
+    $scope.toggleProfanity = function(caller) {
+        $scope.profanity = !$scope.profanity;
+    }
+      $scope.changeView = function (viewNum) {
+          $scope.View = viewNum;
       }
-      $scope.GetLines();
+      $scope.start = function (type) {
+          $scope.changeView(3);
+      }
   }]);
