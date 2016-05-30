4cf76f9fdaa38d3eb88065c3946bb1702e3eedd8
Andrew Johnson <LimeyJohnson@gmail.com>
2016-05-30 09:16:35 -0700

    Moving to Kennedy's House

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,12 +2,13 @@ var hurleyApp = angular.module('hurleyisms', []);
 
 hurleyApp.controller('MainController', ['$scope', '$http', function ($scope, $http) {
     $scope.Lines = [];
+    $scope.CurrentLines = [];
     /*****************************************
                    DataAccess 
     *****************************************/
     function updateCache(callback) {
-        $.getJSON('/data/' + audience + '/' + profanity, function (data) {
-            cachedLines = data;
+        $.getJSON('/data/' + $scope.audience + '/' + $scope.profanity, function (data) {
+            $scope.Lines = data;
             callback();
         });
     }
@@ -23,13 +24,60 @@ hurleyApp.controller('MainController', ['$scope', '$http', function ($scope, $ht
     }
     $scope.View = 1;
     $scope.profanity = false;
-    $scope.toggleProfanity = function(caller) {
+    $scope.toggleProfanity = function (caller) {
         $scope.profanity = !$scope.profanity;
     }
-      $scope.changeView = function (viewNum) {
-          $scope.View = viewNum;
-      }
-      $scope.start = function (type) {
-          $scope.changeView(3);
-      }
-  }]);
+    $scope.changeView = function (viewNum) {
+        $scope.View = viewNum;
+    }
+    $scope.start = function (type) {
+        $scope.audience = type;
+        updateCache(function () {
+            updateLines();
+        });
+    }
+    window.requestAnimFrame = (function () {
+        return window.requestAnimationFrame ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame ||
+          window.oRequestAnimationFrame ||
+          window.msRequestAnimationFrame ||
+          function (/* function */ callback, /* DOMElement */ element) {
+              window.setTimeout(callback, 1000 / 60);
+          };
+    })();
+    var lineIndex = 0;
+    var lineSize = 5;
+    function updateLines() {
+        $scope.$apply(function () {
+            $scope.changeView(3);
+            $scope.CurrentLines = $scope.Lines.slice(lineIndex, lineIndex + lineSize);
+            lineIndex += lineSize;
+        });
+        startProgressBar();
+    }
+    var totalTime = 5 * 1000;
+    var $progressBarFill = $("#progress-bar__fill");
+    var progressBarStartTime;
+    function startProgressBar() {
+        progressBarStartTime = new Date();
+        animateProgressBar();
+        $progressBarFill.width(0);
+    }
+
+    function animateProgressBar() {
+        var elapsedTime = new Date() - progressBarStartTime;
+        if (elapsedTime <= totalTime) {
+            requestAnimFrame(animateProgressBar);
+            drawProgressBar(elapsedTime);
+        }
+        else {
+            updateLines();
+        }
+    }
+    function drawProgressBar(elapsedTime) {
+        var percentage = ((elapsedTime / totalTime) * 100) + "%";
+        $progressBarFill.css("width", percentage)
+    }
+
+}]);
